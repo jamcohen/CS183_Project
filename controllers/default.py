88c41bdbb02e474aacc2cd08e1658182979af2c9
disbelieph <ephraim.chu@gmail.com>
2013-11-21 02:34:08 -0800

    Refactor
    Completely refactored first commit, now works with original schema.

--- controllers/default.py
+++ controllers/default.py
@@ -2,23 +2,35 @@
 # this file is released under public domain and you can use without limitations
 
 #########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
+# # This is a sample controller
+# # - index is the default action of any application
+# # - user is required for authentication and authorization
+# # - download is for downloading files uploaded in the db (does streaming)
+# # - call exposes all registered services (none by default)
 #########################################################################
 
 
 def index():
+    
     response.flash = T("Welcome to web2py!")
     response.menu = [['Home', False, URL('default', 'index')],
-                     ['Make A Menu', False, URL('default', 'construction')],
+                     ['Make A Menu', False, URL('default', 'create_menu')],
                      ['Make A Schedule', False, URL('default', 'construction')],
-                     ['Make A Dish', False, URL('default', 'make_dish')]]
+                     ['Make A Dish', False, URL('default', 'make_dish')],
+                     ['View My Menus', False, URL('default', 'my_menus')]]
     return dict(menu="yolo")
 
+@auth.requires_login()
 def make_dish():
+    
+    response.flash = T("Welcome to web2py!")
+    response.menu = [['Home', False, URL('default', 'index')],
+                     ['Make A Menu', False, URL('default', 'create_menu')],
+                     ['Make A Schedule', False, URL('default', 'construction')],
+                     ['Make A Dish', False, URL('default', 'make_dish')],
+                     ['View My Menus', False, URL('default', 'my_menus')]]
+
+    db.dish.category.requires = IS_IN_SET(['Appetizer', 'Entree', 'Dessert'])
     form = SQLFORM(db.dish, fields = ['name', 'description', 'price', 'ingredients',
                                        'category', 'vegetarian', 'vegan', 'gluten_free'])
     if form.process().accepted:
@@ -27,8 +39,8 @@ def make_dish():
     return dict(form=form)
 
 def view_dish():
-    Dish = db.dish(request.args(0,cast=int))
-    return dict(Dish=Dish)
+    dish = db.dish(request.args(0,cast=int))
+    return dict(dish=dish)
 
 def all_dishes():
     q = db.dish
@@ -37,8 +49,79 @@ def all_dishes():
            csv = False
            )
     return dict(grid=grid)
-    
 
+@auth.requires_login() 
+def create_menu():
+    
+    response.flash = T("Welcome to web2py!")
+    response.menu = [['Home', False, URL('default', 'index')],
+                     ['Make A Menu', False, URL('default', 'create_menu')],
+                     ['Make A Schedule', False, URL('default', 'construction')],
+                     ['Make A Dish', False, URL('default', 'make_dish')],
+                     ['View My Menus', False, URL('default', 'my_menus')]]
+    
+    db.menu.appetizer.requires = IS_IN_DB(db(db.dish.category=="Appetizer"), db.dish.id, '%(name)s')
+    db.menu.entree.requires = IS_IN_DB(db(db.dish.category=="Entree"), db.dish.id, '%(name)s')
+    db.menu.dessert.requires = IS_IN_DB(db(db.dish.category=="Dessert"), db.dish.id, '%(name)s')
+    form = SQLFORM(db.menu, fields = ['name', 'appetizer', 'entree', 'dessert',
+                                       'serving_number', 'delivery_time', 'frequency'],)
+    if form.process().accepted:
+        
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+       response.flash = 'please fill the form'
+   # Note: no form instance is passed to the view
+    return dict(form=form)
+
+def get_dish_name(id):
+    name = db.dish(id).name
+    return name
+
+@auth.requires_login() 
+def my_menus():
+    
+    response.flash = T("Welcome to web2py!")
+    response.menu = [['Home', False, URL('default', 'index')],
+                     ['Make A Menu', False, URL('default', 'create_menu')],
+                     ['Make A Schedule', False, URL('default', 'construction')],
+                     ['Make A Dish', False, URL('default', 'make_dish')],
+                     ['View My Menus', False, URL('default', 'my_menus')]]
+    
+    email = auth.user.email
+    mymenus = db(db.menu.user_id == email).select()
+    for menu in mymenus:
+        menu.update_record(appetizer_name=get_dish_name(menu.appetizer))
+        menu.update_record(entree_name=get_dish_name(menu.entree))
+        menu.update_record(dessert_name=get_dish_name(menu.dessert))
+        print menu.appetizer
+    return dict(mymenus=mymenus)
+
+
+@auth.requires_login() 
+def view_menu():
+    
+    response.flash = T("Welcome to web2py!")
+    response.menu = [['Home', False, URL('default', 'index')],
+                     ['Make A Menu', False, URL('default', 'create_menu')],
+                     ['Make A Schedule', False, URL('default', 'construction')],
+                     ['Make A Dish', False, URL('default', 'make_dish')],
+                     ['View My Menus', False, URL('default', 'my_menus')]]
+    
+    menu = db.menu(request.args(0))
+    email = auth.user.email
+    if menu.user_id == email:
+        return dict(menu=menu)
+    else:
+        session.flash = 'invalid request'
+        redirect(URL('default', 'index'))
+        
+def all_menus():
+   
+   menus = SQLFORM.grid(db.menu)
+   return locals()
+        
 
 def user():
     """
