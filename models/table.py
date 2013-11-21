4e250826f191986dc9f2a402530b5b7fd2ade622
disbelieph <ephraim.chu@gmail.com>
2013-11-20 23:53:21 -0800

    First commit
    Added make a menu function, refactored code to build functionality.
    Added views and other small things

--- models/table.py
+++ models/table.py
@@ -1,21 +1,47 @@
-db.define_table('dish',
+def get_email():
+    if auth.user:
+        return auth.user.email
+    else:
+        return 'None'
+    
+db.define_table('appetizer',
                 Field('name'),
                 Field('description', 'text'),
-                Field('cost', decimal(3,2)),
+                Field('price', 'decimal(3,2)'),
                 Field('ingredients', 'list:string'),
-                Field('category', 'String'),
+                Field('vegetarian', 'boolean'),
+                Field('vegan', 'boolean'),
+                Field('gluten_free', 'boolean')
+                )
+
+db.define_table('entree',
+                Field('name'),
+                Field('description', 'text'),
+                Field('price', 'decimal(3,2)'),
+                Field('ingredients', 'list:string'),
+                Field('vegetarian', 'boolean'),
+                Field('vegan', 'boolean'),
+                Field('gluten_free', 'boolean')
+                )
+
+db.define_table('dessert',
+                Field('name'),
+                Field('description', 'text'),
+                Field('price', 'decimal(3,2)'),
+                Field('ingredients', 'list:string'),
                 Field('vegetarian', 'boolean'),
                 Field('vegan', 'boolean'),
                 Field('gluten_free', 'boolean')
                 )
 
 db.define_table('menu',
-                Field('appetizer', db.dish),
-                Field('entree', db.dish),
-                Field('dessert',  db.dish),
-                Field('name', 'String'),
-                Field('serving_number', 'boolean'), #number of people this menu should serve
+                Field('appetizer'),
+                Field('entree'),
+                Field('dessert'),
+                Field('name'),
+                Field('serving_number', 'integer'), #number of people this menu should serve
                 Field('delivery_time', 'datetime'),
                 Field('frequency', 'integer'), #number of days between deliveries ex. 7 = weekly deliveries
-                Field('user', db.auth_user)
+                Field('user_id', default = get_email(), writable = False)
                 )
+
