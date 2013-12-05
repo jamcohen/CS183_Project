3cce5733a5f411a1eaa476bdba0bd2fb41fde6d7
disbelieph <ephraim.chu@gmail.com>
2013-12-04 19:06:57 -0800

    added weights forms still debugging

--- models/table.py
+++ models/table.py
@@ -3,45 +3,41 @@ def get_email():
         return auth.user.email
     else:
         return 'None'
-    
-db.define_table('appetizer',
+
+def get_dish_name(id):
+    name = db.dish(id).name
+    return name
+
+db.define_table('dish',
                 Field('name'),
                 Field('description', 'text'),
                 Field('price', 'decimal(3,2)'),
+                Field('category'),
                 Field('ingredients', 'list:string'),
+                Field('ingredientWeights', 'list:integer'),
+                Field('weightsMeasurements', 'list:string'),
                 Field('vegetarian', 'boolean'),
                 Field('vegan', 'boolean'),
                 Field('gluten_free', 'boolean')
                 )
 
-db.define_table('entree',
-                Field('name'),
-                Field('description', 'text'),
-                Field('price', 'decimal(3,2)'),
-                Field('ingredients', 'list:string'),
-                Field('vegetarian', 'boolean'),
-                Field('vegan', 'boolean'),
-                Field('gluten_free', 'boolean')
-                )
-
-db.define_table('dessert',
-                Field('name'),
-                Field('description', 'text'),
-                Field('price', 'decimal(3,2)'),
-                Field('ingredients', 'list:string'),
-                Field('vegetarian', 'boolean'),
-                Field('vegan', 'boolean'),
-                Field('gluten_free', 'boolean')
-                )
 
 db.define_table('menu',
-                Field('appetizer'),
-                Field('entree'),
-                Field('dessert'),
+                Field('appetizer',db.dish),
+                Field('entree',db.dish),
+                Field('dessert',db.dish),
                 Field('name'),
                 Field('serving_number', 'integer'), #number of people this menu should serve
                 Field('delivery_time', 'datetime'),
                 Field('frequency', 'integer'), #number of days between deliveries ex. 7 = weekly deliveries
                 Field('user_id', default = get_email(), writable = False)
                 )
+
+db.define_table('deliveries',
+                Field('menu', db.menu),
+                Field('delivery_time', 'datetime'),
+                Field('user_id', default = get_email(), writable = False)
+                )
+
+
 
